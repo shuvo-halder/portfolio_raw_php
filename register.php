60e52b994a7a55c22ebbddd0a6bd07ae942f208c
SHUVO HALDER <samshuvo00@gmail.com>
2024-11-29 18:41:34 +0600

    register validation

--- register.php
+++ register.php
@@ -1,7 +1,60 @@
 <?php
-
 require('header.php');
 include('nav.php');
+
+$name = $email = $password = $confirm_password = "";
+$name_err = $email_err = $password_err = $confirm_password_err = "";
+$ret_name = $ret_email = $ret_pass = $ret_con_pas = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($_POST['name'])) {
+        $name_err = 'Name Required!';
+        
+    }else {
+        if (strlen($_POST['name']) < 6) {
+            $name_err = 'what is your real name?';
+            $ret_name = $_POST['name'];
+        }else{
+            $name = $_POST['name'];
+        }
+    }
+    if (empty($_POST['email'])) {
+        $email_err = 'email Required!';
+        
+    }else {
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $email_err = 'useable email provide';
+            $ret_email = $_POST['email'];
+        }else {
+            $email = $_POST['email'];
+        }
+    }
+    if (empty($_POST['password'])) {
+        $password_err = 'password Required!';
+        
+    }else {
+        if (strlen($_POST['password']) < 6) {
+            $password_err = 'at last 6 chacrecter password required!';
+            $ret_pass = $_POST['password'];
+        }else {
+            $password = $_POST['password'];
+        }
+    }
+    if (empty($_POST['confirm'])) {
+        $confirm_password_err = 'confirm password Required!';
+        $ret_con_pas = $_POST['confirm'];
+    }else {
+        if ($_POST['password'] == $_POST['confirm']) {
+            $confirm_password = $_POST['confirm'];
+        }else {
+            $confirm_password_err = 'password is not match';
+            $ret_con_pas = $_POST['confirm'];
+        }
+        
+    }
+}
+
+
 ?>
 
 <div class="container " style="margin-top: 100px;">
@@ -9,22 +62,26 @@ include('nav.php');
         <div class="col-md-6 m-auto">
             <div class="card">
                 <card class="card-body">
-                    <form action="" class=" text-dark" method="post">
+                    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" class=" text-dark" method="post">
                         <div class="form-group">
                             <label for="" class="text-center">Name <span style="color:red;"> *</span></label>
-                            <input type="text" name="name" class="form-control" placeholder="Your name">
+                            <input type="text" name="name" class="form-control" value="<?= $ret_name; ?>" placeholder="Your name">
+                            <span class="text-danger"><?= $name_err ?></span>
                         </div>
                         <div class="form-group">
                             <label for="" class="text-center">Email<span style="color:red;"> *</span></label>
-                            <input type="email" name="email" class="form-control" placeholder="Your email">
+                            <input type="email" name="email" class="form-control" value="<?= $ret_email; ?>" placeholder="Your email">
+                            <span class="text-danger"><?= $email_err ?></span>
                         </div>
                         <div class="form-group">
                             <label for="" class="text-center">Password <span style="color:red;"> *</span></label>
-                            <input type="password" name="password" class="form-control" placeholder="Your password">
+                            <input type="password" name="password" class="form-control" value="<?= $ret_pass; ?>" placeholder="Your password">
+                            <span class="text-danger"><?= $password_err ?></span>
                         </div>
                         <div class="form-group">
                             <label for="" class="text-center">Confirm Password <span style="color:red;"> *</span></label>
-                            <input type="password" name="confirm" class="form-control" placeholder="Your password">
+                            <input type="password" name="confirm" class="form-control" value="<?= $ret_con_pas; ?>" placeholder="Your confirm password">
+                            <span class="text-danger"><?= $confirm_password_err ?></span>
                         </div>
                         <br>
                         <div class="d-grid gap-2 col-6 mx-auto">
